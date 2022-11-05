6fb092f3b5048857265698b004b1c838da84cb8a
BoD <BoD@JRAF.org>
2022-11-05 15:16:10 +0100

    De-emojify, ignore messages from other users, and de-urlify

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '/tmp/env/.env' });
+    require('dotenv').config({path: '/tmp/env/.env'});
 }
 
 module.exports = {
-  IRC_SERVER:             process.env.IRC_SERVER,
-  IRC_PORT:               parseInt(process.env.IRC_PORT, 10),
-  IRC_SECURE:             !!parseInt(process.env.IRC_SECURE, 10),
-  IRC_NICK:               process.env.IRC_NICK,
-  IRC_PASSWORD:           process.env.IRC_PASSWORD,
-  IRC_CHANNEL:            process.env.IRC_CHANNEL,
-  SLACK_CHANNEL:          process.env.SLACK_CHANNEL,
-  SLACK_TOKEN:            process.env.SLACK_TOKEN,
-  SLACK_APP_TOKEN:        process.env.SLACK_APP_TOKEN,
-  NODE_ENV:               process.env.NODE_ENV,
+    IRC_SERVER: process.env.IRC_SERVER,
+    IRC_PORT: parseInt(process.env.IRC_PORT, 10),
+    IRC_SECURE: !!parseInt(process.env.IRC_SECURE, 10),
+    IRC_NICK: process.env.IRC_NICK,
+    IRC_PASSWORD: process.env.IRC_PASSWORD,
+    IRC_CHANNEL: process.env.IRC_CHANNEL,
+    IRC_SASL: process.env.IRC_SASL === 'true',
+    SLACK_CHANNEL: process.env.SLACK_CHANNEL,
+    SLACK_TOKEN: process.env.SLACK_TOKEN,
+    SLACK_APP_TOKEN: process.env.SLACK_APP_TOKEN,
+    SLACK_USER_NAME: process.env.SLACK_USER_NAME,
+    NODE_ENV: process.env.NODE_ENV,
 };
--- src/index.js
+++ src/index.js
@@ -1,72 +1,105 @@
 const CONFIG = require('./config');
 
-var IRCClient = require('irc').Client;
+const IRCClient = require('irc').Client;
 
-var string = require('string');
-var emoji = require('node-emoji');
+const string = require('string');
+const emoji = require('node-emoji');
 
-var ircConfig = {
-  port: CONFIG.IRC_PORT,
-  secure: CONFIG.IRC_SECURE,
-  password: CONFIG.IRC_PASSWORD,
-  sasl: CONFIG.IRC_SASL,
-  channels: [CONFIG.IRC_CHANNEL]
+const ircConfig = {
+    port: CONFIG.IRC_PORT,
+    secure: CONFIG.IRC_SECURE,
+    password: CONFIG.IRC_PASSWORD,
+    sasl: CONFIG.IRC_SASL,
+    channels: [CONFIG.IRC_CHANNEL]
 };
 
-var ircClient = new IRCClient(CONFIG.IRC_SERVER, CONFIG.IRC_NICK, ircConfig);
+const ircClient = new IRCClient(CONFIG.IRC_SERVER, CONFIG.IRC_NICK, ircConfig);
 
-const { App } = require('@slack/bolt');
+const {App} = require('@slack/bolt');
 
 const slackApp = new App({
-  token: CONFIG.SLACK_TOKEN,
-  appToken: CONFIG.SLACK_APP_TOKEN,
-  socketMode: true
+    token: CONFIG.SLACK_TOKEN,
+    appToken: CONFIG.SLACK_APP_TOKEN,
+    socketMode: true
 });
 
-slackApp.message(async ({ message, say }) => {
-  var text = emoji.emojify(string(message.text).unescapeHTML().toString());
-  ircClient.say(CONFIG.IRC_CHANNEL, text);
+slackApp.message(async ({message}) => {
+    if (message.user !== slackUserId) {
+        return;
+    }
+    const text = emoji.emojify(
+        string(message.text).unescapeHTML().toString()
+            .replaceAll(/<(http.?:\/\/.+?)(\|.+)?>/g, "$1")
+            .replaceAll(":slightly_smiling_face:", ":)")
+            .replaceAll(":disappointed:", ":(")
+            .replaceAll(":smile:", ":D")
+            .replaceAll(":stuck_out_tongue:", ":P")
+            .replaceAll(":open_mouth:", ":O")
+            .replaceAll(":wink:", ";)")
+            .replaceAll(":cry:", ":'("),
+        null,
+        null
+    );
+    ircClient.say(CONFIG.IRC_CHANNEL, text);
 });
 
 (async () => {
-  await slackApp.start();
+    await slackApp.start();
 })();
 
-ircClient.once('raw', function (message) {
-  console.log('Now connected to IRC');
+ircClient.once('raw', function () {
+    console.log('Now connected to IRC');
 });
 
 ircClient.on('message', function (user, channel, text) {
-  if (user === CONFIG.IRC_NICK) {
-    return;
-  }
+    if (user === CONFIG.IRC_NICK) {
+        return;
+    }
 
-  slackApp.client.chat.postMessage({
-    username: user,
-    text: text,
-    channel: conversationId,
-  });
+    void slackApp.client.chat.postMessage({
+        username: user,
+        text: text,
+        channel: conversationId,
+    });
 });
 
 ircClient.on('error', function (error) {
-  console.error(error);
+    console.error(error);
 });
 
 
-var conversationId;
+let conversationId;
 
 async function findConversation(name) {
-  try {
-    const result = await slackApp.client.conversations.list({types: "public_channel,private_channel"});
-    for (const channel of result.channels) {
-      if (channel.name === name) {
-        conversationId = channel.id;
-        break;
-      }
+    try {
+        const result = await slackApp.client.conversations.list({types: "public_channel,private_channel"});
+        for (const channel of result.channels) {
+            if (channel.name === name) {
+                conversationId = channel.id;
+                break;
+            }
+        }
+    } catch (error) {
+        console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
 }
 
-findConversation(CONFIG.SLACK_CHANNEL);
+void findConversation(CONFIG.SLACK_CHANNEL);
+
+let slackUserId;
+
+async function findUser(name) {
+    try {
+        const result = await slackApp.client.users.list();
+        for (const user of result.members) {
+            if (user.name === name) {
+                slackUserId = user.id;
+                break;
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+void findUser(CONFIG.SLACK_USER_NAME);
